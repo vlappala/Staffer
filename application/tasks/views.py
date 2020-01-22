5cae7f1ba29f52df8852d1e3467218a8ccedfeea
Ville Lappalainen <ville.lappalainen@helsinki.fi>
2020-01-22 17:30:32 +0200

    Viikon 2 tehtävät

--- application/tasks/views.py
+++ application/tasks/views.py
@@ -1,10 +1,23 @@
 from application import app, db
-from flask import render_template, request
+from flask import redirect, render_template, request, url_for
 from application.tasks.models import Task
+
+@app.route("/tasks/", methods=["GET"])
+def tasks_index():
+    return render_template("tasks/list.html", tasks = Task.query.all())
 
 @app.route("/tasks/new/")
 def tasks_form():
     return render_template("tasks/new.html")
+  
+@app.route("/tasks/<task_id>/", methods=["POST"])
+def tasks_set_done(task_id):
+
+    t = Task.query.get(task_id)
+    t.done = True
+    db.session().commit()
+  
+    return redirect(url_for("tasks_index"))
 
 @app.route("/tasks/", methods=["POST"])
 def tasks_create():
@@ -13,4 +26,4 @@ def tasks_create():
     db.session().add(t)
     db.session().commit()
   
-    return "hello world!"
+    return redirect(url_for("tasks_index"))
