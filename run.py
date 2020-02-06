43b91554849cb5d7daef11544251f5508e19e446
Ville Lappalainen <ville.lappalainen@helsinki.fi>
2020-02-06 22:31:28 +0200

    Testing heroku

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 from application import app
 
+# Ei toimi??? Pitäisi:
+
+# app.logger.addHandler(logging.StreamHandler(sys.stdout))
+# app.logger.setLevel(logging.ERROR)
+
 if __name__ == '__main__':
     app.run(debug=True)
