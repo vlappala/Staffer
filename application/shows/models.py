375acc6fa064d13be32f134f63194252a7e57dac
Ville Lappalainen <ville.lappalainen@helsinki.fi>
2020-02-07 20:30:49 +0200

    Small usability improvements

--- application/shows/models.py
+++ application/shows/models.py
@@ -27,7 +27,7 @@ class Show(db.Model):
   
         response = []
         for row in res:
-            response.append({"id":row[0], "name":row[1]})
+            response.append({"LKM":row[2], "name":row[1]})
 
         return response
 
